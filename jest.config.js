a9fc3caf97c31cb4795d5d980b04a9ecefc1e5cd
Yuri Gomes <yuurig@gmail.com>
2020-12-05 14:17:11 -0300

    chore: modified config jest

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,6 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/'],
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest',
-  },
+    '.+\\.ts$': 'ts-jest'
+  }
 }
